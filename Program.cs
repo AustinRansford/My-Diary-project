addd354a3355dd9040d56046506e9dc40b90ec1d
AustinRansford <Aransford2024@chadwickschool.org>
2022-07-06 10:23:54 -1000

    light work

--- Program.cs
+++ Program.cs
@@ -1,22 +1,59 @@
 ﻿using System;
-
-namespace My_Diary_project
+using System.IO;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.Serialization;
+namespace My_Diary_proje 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-
-
-
-
-             static void JournalEntry(string entry)
-            {
-                list<string> Journal = 
-            }
+            Start();
         }
-    
+
+        static void Start()
+        {
+            Console.WriteLine("Welcome to your Personal Journal!\nIf you would like to write an entry or read an entry?\n Select 'write' or 'read'.");
+            string FirstResponse = Console.ReadLine();
+            if (FirstResponse.ToLower() == "write")
+            {
+                WriteJournalEntry();
+            }
+            else if (FirstResponse.ToLower() == "read")
+            {
+                ReadJournalEntry();
+            }
+            else
+            {
+                Start();
+            }
+
+        }
+
+
+        static void WriteJournalEntry()
+        {
+            Console.WriteLine("You have chosen to write an entry.\nWhat is today's date?\n return a date in the MM/DD/YEAR:");
+            string CurrentDate = Console.ReadLine();
+            Console.WriteLine("Enter your entry now!");
+            string entry = Console.ReadLine();
+            string path = $@"\JournalEntries\JournalEntry{CurrentDate}.txt";
+           File.CreateText(path);
+        
+
+
+
+
+        }
+
+
+        static void ReadJournalEntry()
+        {
+
+        }
+
+
     }
 }
