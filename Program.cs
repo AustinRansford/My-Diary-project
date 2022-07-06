43430188e66ef81f0e44e100ba3a9a4ee0fb8d18
AustinRansford <Aransford2024@chadwickschool.org>
2022-07-06 12:35:51 -0700

    begining of project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace My_Diary_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+             static void JournalEntry(string entry)
+            {
+                list<string> Journal = 
+            }
+        }
+    
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My-Diary-project
+## methods
+ - take journal entry 
+ - assign journal entry to a date 
+ - method record current date and time.
+ - create a method that can take a date input and return the corresponding journal entry
